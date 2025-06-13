4891981108bac4b8ab5ce45155571ae73fc2a69c
yukimecox <kewong232@gmail.com>
2025-06-12 21:23:43 -0700

    basic server up, minor views file structure changes, see TODOs

--- src/routes/root.ts
+++ src/routes/root.ts
@@ -1,1 +1,10 @@
-console.log("something");
+import express from "express";
+import { Request, Response } from "express";
+
+const router = express.Router();
+
+router.get("/", (_request: Request, response: Response) => {
+  response.render("home");
+});
+
+export default router;
--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,21 @@
-console.log("something");
+import * as path from "path";
+
+import express from "express";
+
+import dotenv from "dotenv";
+dotenv.config();
+
+import * as routes from "./routes";
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.set("views", path.join(process.cwd(), "views"));
+app.set("view engine", "ejs");
+
+app.use("/", routes.root);
+
+app.listen(PORT, () => {
+  console.log(`Listening from http://localhost:${PORT}`);
+});
